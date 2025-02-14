f3f150411206c91bdf3afab08ba0c36037e5eb15
Seonjin Lee <sjbase@me.com>
2025-02-14 00:28:50 -0500

    init

---
+++ github-dark-default.js
@@ -0,0 +1,28 @@
+black       = '#484f58';
+red         = '#ff7b72';
+green       = '#3fb950';
+yellow      = '#d29922';
+blue        = '#58a6ff';
+magenta     = '#bc8cff';
+cyan        = '#39c5cf';
+white       = '#b1bac4';
+lightBlack  = '#6e7681';
+lightRed    = '#ffa198';
+lightGreen  = '#56d364';
+lightYellow = '#e3b341';
+lightBlue   = '#79c0ff';
+lightMagenta= '#d2a8ff';
+lightCyan   = '#56d4dd';
+lightWhite  = '#ffffff';
+
+
+t.prefs_.set('color-palette-overrides',
+                    [ black , red     , green  , yellow,
+                    blue     , magenta , cyan   , white,
+                    lightBlack   , lightRed  , lightGreen , lightYellow,
+                    lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#2f81f7');
+t.prefs_.set('foreground-color', '#e6edf3');
+t.prefs_.set('background-color', '#0d1117');
+
